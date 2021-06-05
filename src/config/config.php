8918e20244f9bd97d8db6be64862313476c8ed77
Muhittin GÜLAP <gulapmuhittin@gmail.com>
2021-06-05 20:36:56 +0300

    xml load setup
    xml load setup

--- src/config/config.php
+++ src/config/config.php
@@ -2,24 +2,37 @@
 
 namespace MGXML\Config;
 
-class config {
-    
-    private $url="XML_URL";
+class config
+{
+
+    private $url = "XML_URL";
 
     public function __construct()
     {
 
     }
-   
-    protected function setUrl($data="")
+
+    protected function setUrl($data = "")
     {
-        $this->url=(string)$data;
+        $this->url = (string) $data;
         return $this;
     }
-  
-    protected function call(){
-        
-        return $return;
+
+    protected function getXmlString()
+    {
+        return (string) $this->xml;
+    }
+
+    protected function getXmlArray()
+    {
+        $this->jsonArray = json_decode(json_encode($xml), true);
+        return $this->jsonArray;
+    }
+
+    protected function load()
+    {
+        $this->xml = simplexml_load_string($this->url);
+        return $this;
     }
 
 }
--- src/libraries/xml.php
+++ src/libraries/xml.php
@@ -4,22 +4,27 @@ namespace MGXML\Libraries;
 
 use MGXML\config\config;
 
-class xml extends config{
-    
+class xml extends config
+{
+
     public function __construct()
     {
         parent::__construct();
     }
 
-    public function getArray(){
-      
-        try{                        
+    public function getArray()
+    {
+        try {
+
+            $return = $this->setUrl()
+                ->load()
+                ->getXmlArray();
 
         } catch (Exception $e) {
-
-        } 
+                die($e->getMessage();
+        }
 
         return $return;
-    }   
-    
+    }
+
 }
